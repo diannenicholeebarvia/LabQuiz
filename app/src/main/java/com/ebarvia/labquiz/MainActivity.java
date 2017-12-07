7a5e8176ebd9b6560de55d9613824f4c7663d77b
diannenicholeebarvia <dianneebarvi@gmail.com>
2017-12-07 18:36:27 +0800

    Initial commit

--- app/src/main/java/com/ebarvia/labquiz/MainActivity.java
+++ app/src/main/java/com/ebarvia/labquiz/MainActivity.java
@@ -3,63 +3,104 @@ package com.ebarvia.labquiz;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText etUser, etPass;
-    Button remember, login;
-    SharedPreferences preferences;
-    private SharedPreferences.Editor loginPrefsEditor;
-
+    EditText et_user;
+    EditText et_pass;
+    Button btn_Rem;
+    Button btn_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUser = (EditText) findViewById(R.id.etUser);
-        etPass = (EditText) findViewById(R.id.etPass);
-        remember = (Button) findViewById(R.id.btnRem);
-        login = (Button) findViewById(R.id.btnLogin);
-        preferences = getPreferences(Context.MODE_PRIVATE);
 
+        et_user = (EditText) findViewById(R.id.etUser);
+        et_pass = (EditText) findViewById(R.id.etPass);
+        btn_Rem = (Button) findViewById(R.id.btnRem);
+        btn_Login = (Button) findViewById(R.id.btnLogin);
+
+        et_user.setOnKeyListener(new EditText.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String user = preferences.getString("username","");
+                String pass = preferences.getString("password","");
+
+                String sUsername = et_user.getText().toString();
+
+                if(!user.isEmpty()) {
+                    if (sUsername.equals(user)) {
+                        et_pass.setText(pass);
+                        et_pass.setBackgroundColor(Color.YELLOW);
+                    }
+                    else if (!(sUsername.equals(user))){
+                        et_pass.setText("");
+                        et_pass.setBackgroundColor(Color.TRANSPARENT);
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        et_pass.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    String user = preferences.getString("username","");
+                    String pass = preferences.getString("password","");
+
+                    String sUsername = et_user.getText().toString();
+
+                    if(!user.isEmpty()) {
+                        if (sUsername.equals(user)) {
+                            et_pass.setText(pass);
+                            et_pass.setBackgroundColor(Color.YELLOW);
+                        }
+                        else if (!(sUsername.equals(user))){
+                            et_pass.setText("");
+                            et_pass.setBackgroundColor(Color.TRANSPARENT);
+                        }
+                    }
+
+                }
+            }
+        });
     }
 
-    public void rememberMe(View view) {
+    public void rememberMe (View view){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("username", etUser.getText().toString());
-        editor.putString("password", etPass.getText().toString());
+        editor.putString("username", et_user.getText().toString());
+        editor.putString("password", et_pass.getText().toString());
         editor.commit();
-        Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Preference Saved!", Toast.LENGTH_SHORT).show();
+
     }
 
-
-    public void MainActivity (View view) {
+    public void login (View view){
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
-
-    public void nextActivity(View view) {
-        String username = String.valueOf(etUser.getText());
-        String password = String.valueOf(etPass.getText());
-
-        // Validates the User name and Password for admin, admin
-        if (username.equals("admin") && password.equals("admin")) {
-            Intent intent = new Intent(this, SecondActivity.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            Toast.makeText(this, "Login Failure", Toast.LENGTH_SHORT).show();
-        }
-    }
-
 
 
 }
--- app/src/main/java/com/ebarvia/labquiz/SecondActivity.java
+++ app/src/main/java/com/ebarvia/labquiz/SecondActivity.java
@@ -1,20 +1,39 @@
 package com.ebarvia.labquiz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView tv_user;
+    Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
+        tv_user = (TextView) findViewById(R.id.tvUser);
+        btn_logout = (Button) findViewById(R.id.btnLogout);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());;
+        String user = preferences.getString("username","");
+        String pwd = preferences.getString("password","");
+        tv_user.setText(user);
+
     }
 
     public void logout (View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
